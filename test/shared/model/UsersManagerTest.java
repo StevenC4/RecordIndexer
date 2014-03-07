b2eb054158fa827f07c41472f373feccf3f22342
Steven Carroll <stevencarroll90@gmail.com>
2014-03-06 17:45:49 -0700

    Added a test class for each Manager class

---
+++ test/shared/model/UsersManagerTest.java
@@ -0,0 +1,11 @@
+package shared.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Steven
+ * Date: 3/6/14
+ * Time: 5:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UsersManagerTest {
+}
